926c9fb484ae8d25f652c5fa69385b9ce03799a4
saydou sy <saydoumamadousy@gmail.com>
2024-05-25 13:40:07 -0400

    Create database.js

---
+++ database.js
@@ -0,0 +1,35 @@
+import { openDB } from 'idb';
+
+const initDB = async () => {
+  await openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+};
+
+export const putDb = async (content) => {
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, content: content });
+  const result = await request;
+  console.log('Data saved to the database', result);
+};
+
+export const getDb = async () => {
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+  result ? console.log('Data retrieved from the database', result.content) : console.log('Data not found in the database');
+  return result?.content;
+};
+
+initDB();
